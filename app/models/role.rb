4c83cfbed6ff3e3efe750f1989ac7d8aa166bc6d
Ana Zitzow <anasempai89@gmail.com>
2022-06-22 18:42:48 -0700

    commit tree 4633dea83deb5e43a66257c36bd530e519bad2a7

---
+++ app/models/role.rb
@@ -0,0 +1,23 @@
+class Role < ActiveRecord::Base
+  has_many :auditions
+
+  def auditions
+
+  end
+
+  def actors
+
+  end
+
+  def locations
+
+  end
+
+  def lead
+
+  end
+
+  def understudy
+
+  end
+end
